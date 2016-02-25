fa9015dba773e676bcfbfe64c65fda269d092be4
Sharon Rumsey <sharon@rumsey.nz>
2016-02-26 11:29:32 +1300

    test for get wine

---
+++ test/get.js
@@ -0,0 +1,34 @@
+var test = require('tape')
+var request = require('supertest')
+var app = require('../server')
+
+var expectedWine = [ 
+
+					{ "varietal": "chardonnay",
+					    "qty": 6},
+
+					 {"varietal": "merlot",
+					  "qty": 2},
+
+					  {"varietal": "pinot gris",
+					    "qty": 99}
+
+					]
+
+test('/returns the correct wine', function (t) {
+	request(app)
+	  .get('/')
+	  .expect('Content type', /JSON/)
+	  .expect(200)
+	  .end(function (err, res) {
+	  	t.equal(res.type, 'application/json', 'application/json')
+	  	t.equal(res.status, 200, 'status code 200')
+	  	t.equal(res.body.length, 3, 'correct length')
+	  	expectedWine.forEach(function (wine) {
+	  		t.ok(res.body.indexOf(wine.varietal) > -1, 'requested array contains varietal: ' + varietal)
+	  	})
+	  })
+	  t.end()
+})
+
+
