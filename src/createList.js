820b354b59bf2c258b915f81d51727e96c01a9f2
gaynor-landmark <g.l.landmark@gmail.com>
2016-02-26 13:40:58 +1300

    reorganise createlist

--- src/createList.js
+++ src/createList.js
@@ -1,7 +1,16 @@
-// var getRequest = require(./ajax.js/getFromServer)
+var getRequest = require(./getrequest)
 
 module.exports =function(){
 
 console.log("hello from create list")
+getRequest(appendtoList)
 
+}
+
+
+function appendtoList(cellarData){
+  $('#table').text('')
+  cellarData.forEach( (item) => {
+     $('#table').append('<div>' + item.varietal + '</div>')
+  })
 }
